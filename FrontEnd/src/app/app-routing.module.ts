65b1f9bf3ba67a6d9aba1f2186465e3dd365c386
Shivanshu <shivanshu63072@gmail.com>
2024-08-15 20:18:29 +0530

    New FrontEnd Changes.. added one new component Homepage.

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/registration', pathMatch: 'full' },
+  { path: '', redirectTo: '/homepage', pathMatch: 'full' }, // Redirect to homepage
   { path: 'login', component: LoginComponent },
-  { path: 'registration', component: RegistrationComponent }
-
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'homepage', component: HomepageComponent } // Explicit route for homepage
 ];
 
 @NgModule({
